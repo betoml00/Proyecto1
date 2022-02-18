565a4570f275db14c50b2f6ccad3b4b2a930a1cc
Alberto Márquez <50601838+betoml00@users.noreply.github.com>
2022-02-18 08:34:21 -0600

    Cambios de Emilio

--- polinomios.pl
+++ polinomios.pl
@@ -1,28 +1,53 @@
-poly_sum(Ms,[],Ms) :- Ms = [|].
-poly_sum([],Ns,Ns).
-poly_sum([M|Ms], [N|Ns], [S|Ss]) :-
-   S is M+N,
-   poly_sum(Ms, Ns, Ss).
+%SUMA
+%Si B es vacio TODO comentar
+suma_pol(A,[],A) :- A = [_|_].
+%Si A es vacio la suma es igual a B.
+suma_pol([],B,B).
+%Para sumar dos polinomios se suman sus cabezas y llama recursivamente
+suma_pol([Ca|A], [Cb|B], [Cc|C]) :-
+   Cc is Ca+Cb,
+   suma_pol(A, B, C).
 
-scal_prod([],_Sc,[]).
-scal_prod([M|Ms], Sc, [P|Ps]) :-
-   P is M*Sc,
-   scal_prod(Ms, Sc, Ps).
+%RESTA
+%Si B es vacio TODO
+resta_pol(A,[],A) :- A = [_|_].
+%Si A es vacio la suma es igual a B.
+resta_pol([],B,B).
+%Para sumar dos polinomios se suman sus cabezas y llama recursivamente
+resta_pol([Ca|A], [Cb|B], [Cc|C]) :-
+   Cc is Ca-Cb,
+   resta_pol(A, B, C).
 
-poly_prod(_,[],[]).
-poly_prod(Ms,[N|Ns], Xs2) :-
-   poly_prod(Ms,Ns, Xs1),
-   scal_prod(Ms, N, Ps),
-    poly_sum(Ps, [0.0|Xs1], Xs2).
+%PRODUCTO ESCALAR
+%Si el polinomio es vacio su producto Esc tambien.
+producto_Esc_pol([],_,[]).
+%Si es no vacio, se multiplica su cabeza con el Esc y llama rec.
+producto_Esc_pol([Ca|A], Esc, [Cc|C]) :-
+   Cc is Ca*Esc,
+   producto_Esc_pol(A, Esc, C).
 
-% Esto es un saludo al Fer.
+%PRODUCTO
+%Si B es vacio el producto es vacio.
+producto_pol(_,[],[]).
+%Si son no vacios
+producto_pol(A,[Cb|B], C) :-
+   producto_pol(A,B, Rec), %quitamos cabeza de B y llamamos recursivamente.
+   producto_Esc_pol(A, Cb, Esc), %calculamos el prod. Esc con la cabeza de B.
+   suma_pol(Esc, [0.0|Rec], C). %sumamos ambos resultados anteriores en C.
 
+%GRADO
+%Basicamente la longitud de la lista
+grado_pol([],0).
+grado_pol([_|A],Grad):-
+    grado_pol(A,Temp),
+    Grad is Temp+1.
 
-% Otra prueba de Beto.
-/*
-?- poly_prod([1,2,3,4],[5,6,7],Xs).
-Xs = [5.0, 16.0, 34.0, 52, 45, 28] ;
-false.
-*/
+%EVALUAR
+eval_pol([],_,0).
+eval_pol([Ca|A],X,Res):-
+    eval_pol(A,X,Temp),
+    Res is (Temp*X)+Ca.
 
-% Comentario
+%DIFERENCIAR
+dif_pol([],[]).
+dif_pol([Ca|A],)
